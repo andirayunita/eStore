e7673d08dd378f1840cd2c6fa3bd644719f97204
Andira Yunita <andhirayp@gmail.com>
2024-04-02 16:36:39 +0700

    Initial Commit

---
+++ eStore/eStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  eStoreApp.swift
+//  eStore
+//
+//  Created by Andira Yunita on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct eStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
